1dc221acb8373f082d52db45d3970cec71a2ff6a
Rini <Reneortega2468@gmail.com>
2023-12-01 09:08:06 -0500

    socket url

--- app/api/baseUrl.ts
+++ app/api/baseUrl.ts
@@ -4,4 +4,4 @@ export const GOOGLE_REDIRECT_URI = 'https://5fjrs4-3000.csb.app/google'
 
 export const googleAuthEndpoint = `https://accounts.google.com/o/oauth2/v2/auth?client_id=477985071118-h8hj5iagdgq5hnk051nqkbf6b83a11v0.apps.googleusercontent.com&redirect_uri=${GOOGLE_REDIRECT_URI}&response_type=code&scope=openid%20email%20profile`
 
-export const SOCKET_URL = 'ws://192.168.86.41:3000'
+export const SOCKET_URL = 'ws://192.168.0.44:3000'
